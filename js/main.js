89ca945f000ec39ffc06d1612b6485002deba6ff
Supreme_Coder <148485339+tradleytay@users.noreply.github.com>
2025-10-23 18:16:51 +0200

    updates

--- js/main.js
+++ js/main.js
@@ -31,13 +31,15 @@
     $('.btn-play').click(function() {
         $videoSrc = $(this).data("src");
     });
-    console.log($videoSrc);
-    $('#videoModal').on('shown.bs.modal', function(e) {
-        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-    })
-    $('#videoModal').on('hide.bs.modal', function(e) {
-        $("#video").attr('src', $videoSrc);
-    })
+
+    $('#videoModal').on('shown.bs.modal', function() {
+        $("#video").attr('src', $videoSrc + "?autoplay=1&modestbranding=1&showinfo=0");
+    });
+
+    $('#videoModal').on('hide.bs.modal', function() {
+        $("#video").attr('src', "");
+    });
+
 
 
     // Back to top button
